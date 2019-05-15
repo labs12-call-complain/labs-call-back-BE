b3a03257f95fb8a0d71892f26132c58ccd904a75
tactnician <tactnician@gmail.com>
2019-05-15 08:26:59 -0400

    added database configuration file to data folder for easy import to api components

--- config/twitter/twitter-model.js
+++ config/twitter/twitter-model.js
@@ -10,24 +10,43 @@ const client = new Twitter({
 
 
 module.exports = {
-    
-    createTweet : client.post('statuses/update', {status: 'I Love Twitter'})
-        .then(function (tweet) {
-            console.log(tweet);
-        }) //it should return tweet, user and id
-        .catch(function (error) {
-            throw err
-        }),
-    deleteTweet: client.post('statuses/destroy/:id', {id: '',  name: ''})
-        .then()
-        .catch(),
-    getSingleTweet: client.get('statuses/show/:id', {name: ''})
-        .then()
-        .catch(),
 
-         
-    
+    createTweet: async function(tweet) {
+        try {
+            tweetConfirm = await client.post('statuses/update', tweet);
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    deleteTweet: async function(id) {
+        try {
+            
+        } catch (error) {
+            
+        }
+        
+    }
+
+
 }
+
+// module.exports = {
+    
+//     createTweet :  client.post('statuses/update', {status: 'I Love Twitter'})
+//         .then(function (tweet) {
+//             console.log(tweet);
+//         }) //it should return tweet, user and id
+//         .catch(function (error) {
+//             throw err
+//         }),
+//     deleteTweet: client.post('statuses/destroy/:id', {id: '',  name: ''})
+//         .then()
+//         .catch(),
+//     getSingleTweet: client.get('statuses/show/:id', {name: ''})
+//         .then()
+//         .catch(),
+
+// }
 
 
 
--- config/twitter/twitter-test.js
+++ config/twitter/twitter-test.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const Twitter = require('twitter');
+// const db = require('file')
 
 const client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -9,21 +10,40 @@ const client = new Twitter({
 });
 
 
-const getFav = client.get('favorites/list', function(error, tweets, response) {
-    if(error) throw error;
-    console.log(tweets);  // The favorites.
-    console.log(response);  // Raw response object.
-});
+// const getFav = client.get('favorites/list', function(error, tweets, response) {
+//     if(error) throw error;
+//     console.log(tweets);  // The favorites.
+//     console.log(response);  // Raw response object.
+// });
 
-function postTweet() { client.post('statuses/update', {status: "I love twitter"})
-    .then(tweet => {
-        console.log(tweet)
-    })
-    .catch(error => {
-        console.log(error)
-})
+
+const postTweet = async function() {
+    try {
+        tweet = await client.post('statuses/update', {status: "Post from ExpressJS 5"});
+
+        console.log(tweet.id);
+        console.log(tweet.id_str);
+        console.log(tweet.text);
+        console.log(tweet.entities);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
-console.log(getFav);
+    
 
+
+
+
+// function postTweet() { client.post('statuses/update', {status: "I love twitter"})
+//     .then(tweet => {
+//         console.log(tweet)
+//     })
+//     .catch(error => {
+//         console.log(error)
+// })
+// }
+
+
+postTweet();
